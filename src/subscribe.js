e08760c89f8aa7d33c0d166ab0aff79493f9fb8b
poberWong <poberwong@gmail.com>
2016-03-02 16:36:57 +0800

    finish subscribe with email

---
+++ src/subscribe.js
@@ -0,0 +1,18 @@
+import React from 'react'
+require ("./css/subscribe.css")
+export default class Subscribe extends React.Component {
+	render () {
+		return (
+			<div id="subscribe">
+				<form method="post">
+					<div style={{marginBottom: 20}}>
+						<h5>邮箱</h5>
+						<input id="email" type="text" placeholder="输入需要订阅的邮箱"/>
+					</div>
+					<br/>
+					<span><input type="submit" value="订阅!"/>&nbsp;&nbsp;&nbsp;需要您在邮箱点击链接进行验证</span>
+				</form>
+			</div>
+		)
+	}
+}
